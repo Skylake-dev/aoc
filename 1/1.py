89f674c7f89baaf13cbeab4f72ef739176c2946d
Skylake-dev <77413867+Skylake-dev@users.noreply.github.com>
2022-12-01 10:22:53 +0100

    day 1

---
+++ 1/1.py
@@ -0,0 +1,14 @@
+file = open('./1/input')
+elf = 0
+elves_cal = [0]
+for line in file.readlines():
+    if line != '\n':
+        elves_cal[elf] += int(line)
+    else:
+        elves_cal.append(0)
+        elf += 1
+
+print(f'there are {len(elves_cal)} in total')
+elves_cal.sort(reverse=True)
+print(f'Top 3: {elves_cal[0]}, {elves_cal[1]}, {elves_cal[2]}')
+print(f'total = {elves_cal[0]+elves_cal[1]+elves_cal[2]}')
