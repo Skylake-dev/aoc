0e0bde89b654b7ab7bf8c450f38ca59f51141ea6
Skylake-dev <77413867+Skylake-dev@users.noreply.github.com>
2024-12-13 16:26:03 +0100

    [2024] day 12 part 2
    it was not so difficult in the end, i was lazy and
    i did't want to write down all the cases

--- 2024/python/12.py
+++ 2024/python/12.py
@@ -1,9 +1,9 @@
-from time import sleep
 from time import time
 from collections import namedtuple
-from dataclasses import dataclass, field
+from dataclasses import dataclass
 
-with open('../inputs/12.txt') as data:
+with open(r'C:\Users\capof\Documents\Progetti\aoc\2024\inputs\12.txt') as data:
+    # with open('../inputs/12.txt') as data:
     input = data.readlines()
 
 ###############
@@ -48,6 +48,31 @@ def _in_bounds(point: Coords) -> bool:
     return (0 <= point.x <= max_x) and (0 <= point.y <= max_y)
 
 
+def get_same_type_neighbors_horizontally(pos: Coords) -> list[Coords]:
+    """Get the neighbors left and right of this cell, if any."""
+    neighbors = []
+    increments = (-1, 1)
+    # left and right means fix row and move column
+    for dy in increments:
+        cand = Coords(pos.x, pos.y+dy)
+        # short circuit the and if not in bounds
+        if _in_bounds(cand) and plots_map[pos.x][pos.y] == plots_map[cand.x][cand.y]:
+            neighbors.append(cand)
+    return neighbors
+
+
+def get_same_type_neighbors_vertically(pos: Coords) -> list[Coords]:
+    """Get the neighbors above and below this cell, if any."""
+    neighbors = []
+    increments = (-1, 1)
+    # above and below means fix the column and move row
+    for dx in increments:
+        cand = Coords(pos.x+dx, pos.y)
+        if _in_bounds(cand) and plots_map[pos.x][pos.y] == plots_map[cand.x][cand.y]:
+            neighbors.append(cand)
+    return neighbors
+
+
 def get_same_type_neighbors(pos: Coords) -> list[Coords]:
     """Returns neighbors of the current position. A cell can have
     between 0 and 4 neighbors (since we exclude diagonals).
@@ -64,15 +89,8 @@ def get_same_type_neighbors(pos: Coords) -> list[Coords]:
     # all the possible directions, no diagonals
     # check bounds and that the type is the same
     # before appending the neighbor
-    increments = (-1, 1)
-    for dx in increments:
-        cand = Coords(pos.x+dx, pos.y)
-        if _in_bounds(cand) and plots_map[pos.x][pos.y] == plots_map[cand.x][cand.y]:
-            neighbors.append(cand)
-    for dy in increments:
-        cand = Coords(pos.x, pos.y+dy)
-        if _in_bounds(cand) and plots_map[pos.x][pos.y] == plots_map[cand.x][cand.y]:
-            neighbors.append(cand)
+    neighbors += get_same_type_neighbors_horizontally(pos)
+    neighbors += get_same_type_neighbors_vertically(pos)
     return neighbors
 
 
@@ -92,10 +110,10 @@ def get_same_type_unassigned_neighbors(pos: Coords) -> list[Coords]:
 @dataclass
 class Region:
     # define a region, containing different fields
-    # set is not initialized because i was having errors
     plots: set[Coords]          # plots of the garden in this area
     label: str = ''             # the type of plots inside this region
     perimeter: int = 0          # the perimeter, invalid value until the end
+    sides: int = 0              # the number of sides, same as perimeter but merge contiguous
     area: int = 0               # the area, which is just the number of plots
 
 
@@ -114,6 +132,21 @@ def recursive_neighbor_update(region: Region, neighbors: list[Coords]) -> None:
         recursive_neighbor_update(
             region, get_same_type_unassigned_neighbors(cell))
 
+
+def build_contiguous_sections(points: list[int]) -> list[list[int]]:
+    # first extract contiguous blocks by checking delta between points
+    if len(points) == 0:
+        # empty, so return empty list
+        return []
+    contiguous_sections: list[list[int]] = [[points[0]]]
+    for i in range(1, len(points)):
+        if points[i] == points[i-1] + 1:
+            # it's contiguous, keep appending to the current array
+            contiguous_sections[-1].append(points[i])
+        else:
+            # not contiguous, create a new block
+            contiguous_sections.append([points[i]])
+    return contiguous_sections
 
 ###############
 # part 1
@@ -173,7 +206,120 @@ for region in regions:
     # plots, i.e. the ones with less than 4 neighbors
     border_plots = list(filter(lambda p: len(
         get_same_type_neighbors(p)) < 4, region.plots))
-    # need to figure out logic
+    # ok this is a weird idea, but
+    # i could divide the points in two dictionary, one
+    # indexed by the x coord and the other by the y coord
+    # and then sort the other coordinate and extract contiguous
+    # groups
+    x_map: dict[int, list[int]] = {}
+    y_map: dict[int, list[int]] = {}
+    for plot in border_plots:
+        # add points from the x
+        if plot.x in x_map:
+            x_map[plot.x].append(plot.y)
+        else:
+            x_map[plot.x] = [plot.y]
+        # add points from the y
+        if plot.y in y_map:
+            y_map[plot.y].append(plot.x)
+        else:
+            y_map[plot.y] = [plot.x]
+    # track the number of sides, see comments later to
+    # understand the procedure
+    above_sides: int = 0
+    below_sides: int = 0
+    left_sides: int = 0
+    right_sides: int = 0
+    for x in x_map:
+        # sort them all in order of coordinates, so that it would
+        # be easier to extract the contiguous plots
+        points = x_map[x]
+        points.sort()
+        # assume they all form one side and split them when needed
+        # e.g. in a square region for one i have either the top or bottom
+        # side not both in the same lines
+        # this is used to handle plots that protrude out, for instance when
+        # considering this region
+        # S S S S
+        # S S S
+        # check the first line, both above and below
+        # _ _ _ _   --> 1 contiguous side
+        # S S S S
+        # x x x _   --> 1 contiguous side but short, blocked by neighbors below
+        # S S S
+        # but for the second line, the side above are all blocked
+        # S S S S
+        # x x x     --> 1 contiguous side but blocked by neighbors above, so no side is added
+        # S S S
+        # _ _ _     --> 1 contiguous side
+        contiguous_plots = build_contiguous_sections(points)
+        # now check the neigbors for blocking for each of the contiguous plots
+        # blockings will cause more splits within each contiguous side
+        # the blocking is tracked in a list of booleans, and this will be used to
+        # determine the split
+        # e.g.
+        # blocked_above = [True, False, False]
+        # blocked_below = [False, False, False]
+        # represents something like
+        #  S
+        #  S S S   <-- considering this line
+        # so the considered line will have one side above and one below
+        # a split is caused by a section of True between False
+        # e.g.
+        # blocked_above = [False, True, False]
+        # blocked_below = [False, False, False]
+        # represents something like
+        #    S
+        #  S S S   <-- considering this line
+        # below there will be a single side like before, but above
+        # i have a split caused by the S in the middle, so it will
+        # produce 2 sides (for this line, the third side caused by the
+        # above S will be handled by that line)
+
+        for section in contiguous_plots:
+            blocked_above: list[bool] = []
+            blocked_below: list[bool] = []
+            for cell_y in section:
+                # only interested in the x coordinate
+                vertical_neighbors: list[int] = list(
+                    map(lambda pos: pos.x, get_same_type_neighbors_vertically(Coords(x, cell_y))))
+                # track blocked cells above and below (remember the weird axis of row column)
+                blocked_below.append((x + 1) in vertical_neighbors)
+                blocked_above.append((x - 1) in vertical_neighbors)
+            # now produce the split of this section, counting the sides
+            # to produce the splits filter out the elements that are blocked
+            # do the splits
+            unblocked_cells_above = [section[i] for i in range(
+                len(section)) if not blocked_above[i]]
+            unblocked_cells_below = [section[i] for i in range(
+                len(section)) if not blocked_below[i]]
+            # if it was not split, this should only add 1, if all are blocked then
+            # no side is counted for that part
+            above_sides += len(build_contiguous_sections(unblocked_cells_above))
+            below_sides += len(build_contiguous_sections(unblocked_cells_below))
+    # do the same thing on y but check left/right neighbors instead
+    # see the logic in the previous comments
+    for y in y_map:
+        points = y_map[y]
+        points.sort()
+        contiguous_plots = build_contiguous_sections(points)
+        for section in contiguous_plots:
+            blocked_left: list[bool] = []
+            blocked_right: list[bool] = []
+            for cell_x in section:
+                # only interested in the y coordinate this time
+                horizontal_neighbors: list[int] = list(
+                    map(lambda pos: pos.y, get_same_type_neighbors_horizontally(Coords(cell_x, y))))
+                blocked_right.append((y + 1) in horizontal_neighbors)
+                blocked_left.append((y - 1) in horizontal_neighbors)
+            unblocked_cells_left = [section[i] for i in range(
+                len(section)) if not blocked_left[i]]
+            unblocked_cells_right = [section[i] for i in range(
+                len(section)) if not blocked_right[i]]
+            left_sides += len(build_contiguous_sections(unblocked_cells_left))
+            right_sides += len(build_contiguous_sections(unblocked_cells_right))
+    # the sides of the region is the sum of all of these sides
+    region.sides = above_sides + below_sides + left_sides + right_sides
 
 print(f'[part 2] time: {(time()-begin):.4f}s')
-print(f'part 2: {0}')
+print(f'part 2: {sum([r.area*r.sides for r in regions])}')
